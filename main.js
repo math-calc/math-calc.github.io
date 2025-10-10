c6f18e9373746922db095224ba5b2e555b299b6b
math-calc <krivobokovacila@yandex.com>
2025-10-10 15:48:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+(function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104591869', 'ym');
+
+    ym(104591869, 'init', {ssr:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+<noscript><div><img src="https://mc.yandex.ru/watch/104591869" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
